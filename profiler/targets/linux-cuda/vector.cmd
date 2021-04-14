067c125c72590f42f7043fab14cba9f365dd00e3
Patrick Broderick <Patrick.Broderick@gmail.com>
2021-04-14 10:45:54 -0400

    build out-of-source; in-source fails on bridges

--- profiler/targets/linux-cuda/vector.cmd
+++ profiler/targets/linux-cuda/vector.cmd
@@ -11,8 +11,10 @@ TEMPDIR=$PWD
 
 cp -f ../../targets/common/CMakeLists.txt $TEMPDIR/CMakeLists.txt
 mv testcode.c testcode.cu
-cmake -DPROJECT:STRING=cvector . > /dev/null
+rm -rf build && mkdir build && cd build
+cmake -DPROJECT:STRING=cvector .. > /dev/null
 make install > /dev/null
+cd ..
 
 if [ -f ./cvector ]; then
     ./cvector > vector.txt

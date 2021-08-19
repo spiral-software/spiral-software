c978564cad3cddce72f8605089cdbfce96e36890
Patrick Broderick <50112491+broderickpt@users.noreply.github.com>
2021-08-19 11:26:09 -0400

    Convert allProfiler Targets to use CMake (#44)
    * Set master branch for doc updates (was develop)
    
    * build out-of-source; in-source fails on bridges
    
    * Add CUDA examples to the user manual
    
    * rebuild docs on push to develop or master branch
    
    * look in develop branch for updates
    
    * Convert all profiler targets to use CMake
    
    * Convert all profiler targets to use CMake & rm obsolete Makefiles
    
    * updates - use cmake for all targets
    
    * adjust libraries and compiler settings (adjusts for OpenMP) / CUDA

--- profiler/targets/win-x64-cuda/time.cmd
+++ profiler/targets/win-x64-cuda/time.cmd
@@ -3,21 +3,18 @@
 REM  Copyright (c) 2018-2021, Carnegie Mellon University
 REM  See LICENSE for details
 
+REM  Use cmake to build the project (PROJECT=time) for CUDA language (SUFFIX=cu) 
+
 set SGBETEMPDIR=%cd%
-
-REM  Taking a simple approach here: copy CMakeLists.txt from win-x64-nvcc
-REM  target to the temp directory; run cmake to configure then build the
-REM  target and finally, if successful, execute the target.
-
 COPY ..\..\targets\common\CMakeLists.txt %SGBETEMPDIR%\CMakeLists.txt
 RENAME testcode.c testcode.cu
-cmake -DPROJECT:STRING=time . < nul
+rm -rf build && md build && cd build
+cmake -DPROJECT:STRING=time -DSUFFIX:STRING=cu .. < nul
 cmake --build . --config Release --target install < nul
+cd ..
 
 IF EXIST .\time.exe (
     .\time.exe > time.txt
 ) ELSE (
     type nul > time.txt
 )
-
-
--- profiler/targets/win-x86-vcc/time.cmd
+++ profiler/targets/win-x86-vcc/time.cmd
@@ -3,14 +3,17 @@
 REM  Copyright (c) 2018-2021, Carnegie Mellon University
 REM  See LICENSE for details
 
-REM For Visual Studio 2017 and later the environment variables are set by: VsDevCmd.bat.  The path
-REM to VsDevCmd.bat can be determined with vswhere.exe, which is (by default) located at:
-REM "C:\Program Files (x86)\Microsoft Visual Studio\Installer" We'll use this to locate
-REM VsDevCmd.bat, then run it to set the environment for nmake...
-
-for /f "usebackq delims=#" %%a in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do call "%%a\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 > nul
+REM  Use cmake to build the project (PROJECT=rdtsc_time) for C language (SUFFIX=c) 
 
 set SGBETEMPDIR=%cd%
-nmake /C /S /f ../../targets/win-x86-vcc/Makefile clean
-nmake /C /S /f ../../targets/win-x86-vcc/Makefile GAP=%SGBETEMPDIR%\testcode.c STUB=%SGBETEMPDIR%\testcode.h CC="cl" OUTDIR=%SGBETEMPDIR% all > time.txt
+COPY ..\..\targets\common\CMakeLists.txt %SGBETEMPDIR%\CMakeLists.txt
+rm -rf build && md build && cd build
+cmake -DPROJECT:STRING=rdtsc_time -DSUFFIX:STRING=c .. < nul
+cmake --build . --config Release --target install < nul
+cd ..
 
+IF EXIST .\rdtsc_time.exe (
+    .\rdtsc_time.exe > time.txt
+) ELSE (
+    type nul > time.txt
+)

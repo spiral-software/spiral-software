ba48ac3008a2ec56329cd30fd073c151731dd4d6
Mike Franusich <mike.franusich@gmail.com>
2021-11-02 15:23:00 -0400

    remote unused iface_tcpip

--- gap/src/iface_table.c
+++ gap/src/iface_table.c
@@ -6,7 +6,7 @@
 #include "iface_console.h"
 #include "iface_batch.h"
 //#include "iface_gui.h"
-//#include "iface_tcpip.h"
+
 //#include "iface_rmi.h"
 
 struct gap_iface gap_interface[IFACE_TABLE_SIZE] =
@@ -14,7 +14,6 @@ struct gap_iface gap_interface[IFACE_TABLE_SIZE] =
    {ID_CONSOLE, "console", console_main, console_printf}, // Working 
    {ID_GUI, "gui", console_main, console_printf},  // Not implemented
    {ID_TCPIP, "tcpip", console_main, console_printf},
-   //{ID_TCPIP, "tcpip", tcpip_main, tcpip_printf},  // kinda working 
    {ID_JAVARMI, "javarmi", console_main, console_printf},  // Not implemented
    {ID_BATCH, "batch", batch_main, batch_printf} // Working
 };

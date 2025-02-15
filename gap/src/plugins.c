1abde5cf9f84df687c1fa71a34629cc6149150e6
Mike Franusich <mike.franusich@gmail.com>
2025-02-15 14:08:38 -0500

    create plugins branch and add key files from experimental cfunc branch

---
+++ gap/src/plugins.c
@@ -0,0 +1,74 @@
+
+//  Copyright (c) 2025, Carnegie Mellon University
+//  See LICENSE for details
+
+//
+// plugins.c -- support for loading plugins
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef WIN32
+    #include "win_dlfcn.h"
+#else
+    #include <dlfcn.h>
+#endif
+
+
+// for GAP to C functions
+#include "system.h"
+#include "memmgr.h" 
+#include "integer.h"
+#include "args.h"
+#include "eval.h"
+
+
+typedef void (*fptr)();
+typedef fptr (*lookup_func)(char *);
+typedef void (*init_func)(lookup_func);
+
+fptr LookupFunctionPointer(char* name) {
+    if (strcmp(name, "InstIntFunc") == 0) {
+        return (fptr)InstIntFunc;
+    }
+    else {
+        return (fptr)0;
+    }
+}
+
+
+Obj FunLoadPlugin(Obj hdCall) {
+    char * usage = "usage: LoadPlugin(<plugin>)";
+    Obj  hd1;
+    char* libname;
+    char* funcname = "init_plugin";
+    void *handle;
+    void *funcptr;
+    
+    if (GET_SIZE_BAG(hdCall) != 2 * SIZE_HD) {
+        return Error(usage, 0, 0);
+    }
+    hd1 = EVAL(PTR_BAG(hdCall)[1]);
+    
+    libname = HdToString(hd1, "<plugin> must be a String.\n%s", usage, 0);
+    
+    handle = dlopen(libname, RTLD_LAZY);
+    if (handle == 0) {
+        return Error("cannot open plugin %s", libname, 0);
+    }
+    
+    funcptr = dlsym(handle, funcname);
+    if (funcptr == 0) {
+        return Error("cannot find function %s in library %s", funcname, libname);
+    }
+    
+    ((init_func)funcptr)(LookupFunctionPointer);
+    
+    return INT_TO_HD(0);
+}
+
+
+void Init_Plugins() {
+    InstIntFunc("LoadPlugin", FunLoadPlugin);
+}
--- gap/src/spiral.c
+++ gap/src/spiral.c
@@ -1406,6 +1406,7 @@ extern void Init_GAP_Utils();
 extern void Init_types();
 extern void Init_Double();
 extern void Init_Complex();
+extern void Init_Plugins();
 
 void            InitSPIRAL (void) {
     InitSPIRAL_Paths();
@@ -1414,6 +1415,7 @@ void            InitSPIRAL (void) {
     InitSPIRAL_BagList();
     Init_Double();
     Init_Complex();
+    Init_Plugins();
     Init_types();
     Init_GAP_Utils();
     /**/ GlobalPackage2("spiral", "util"); /**/

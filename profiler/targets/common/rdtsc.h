2e5123fd77b72250b35f0ceb4aad11a8d62ea421
Patrick Broderick <50112491+broderickpt@users.noreply.github.com>
2025-11-09 13:00:09 -0500

    Update year in Copyright; add release notes for rel 8.5.3 (#141)

--- profiler/targets/common/rdtsc.h
+++ profiler/targets/common/rdtsc.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018-2021, Carnegie Mellon University
+ *  Copyright (c) 2018-2025, Carnegie Mellon University
  *  See LICENSE for details
  */
  
--- profiler/targets/common/time.cu
+++ profiler/targets/common/time.cu
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018-2023, Carnegie Mellon University
+ *  Copyright (c) 2018-2025, Carnegie Mellon University
  *  See LICENSE for details
  */
 

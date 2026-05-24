c957acdf7d47d7d2f05e95fc07df14f15edf5056
Patrick Broderick <Patrick.Broderick@gmail.com>
2026-05-24 19:32:40 -0400

    Updates for removinh helper_cuda

--- support/checkforGpu.cu
+++ support/checkforGpu.cu
@@ -1,15 +1,20 @@
 /*
- **  Copyright (c) 2018-2021, Carnegie Mellon University
+ **  Copyright (c) 2018-2026, Carnegie Mellon University
  **  See LICENSE for details
  */
 
-#include <helper_cuda.h>
+#include <stdio.h>
+#include <cuda_runtime.h>               // Standard CUDA header
 
 int main ( int argc, char* argv[] )
 {
-	int deviceCount;
-	cudaGetDeviceCount(&deviceCount);
-	printLastCudaError("looking for CUDA devices: "); //cudaGetLastError()
+	int deviceCount = 0;
+	cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+	if (err != cudaSuccess) {
+		fprintf(stderr, "looking for CUDA devices: %s\n", cudaGetErrorString(err));
+		return (-1);
+	}
 
 	if (deviceCount == 0) {
 		printf("No GPU devices found, exit abnormally\n");
